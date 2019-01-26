961617264c3ac10012dc48b11511e672412fa4db
Thundercleez <tmizas@gmail.com>
2019-01-26 16:53:50 -0500

    added camera following player on x axis.

--- BongoCat/Assets/Scripts/CamController.cs
+++ BongoCat/Assets/Scripts/CamController.cs
@@ -12,11 +12,14 @@ public class CamController : MonoBehaviour {
     float maxSpeed;
     [SerializeField]
     float loseThreshold;
+    [SerializeField]
+    float followSpeed;
 
     [SerializeField]
     GameObject loseText;
 
     float curSpeed;
+    bool following;
 
 	// Use this for initialization
 	void Start () {
@@ -29,6 +32,23 @@ public class CamController : MonoBehaviour {
         float camSpeed = Mathf.Clamp(Mathf.Pow(curSpeed, 2) + Mathf.Log(curSpeed) + 2, startSpeed, maxSpeed);
 
         gameObject.transform.position += Vector3.up * camSpeed;
+        Vector3 screenPos = Globals.Instance.mainCam.WorldToScreenPoint(Globals.Instance.playerObj.transform.position);
+        float xOffset = Screen.width * .5f - screenPos.x;
+        if(following ||
+            Mathf.Abs(xOffset) > Screen.width * .13f)
+        {
+            following = true;
+            Vector3 tarDir = Globals.Instance.playerObj.transform.position - gameObject.transform.position;
+            tarDir.Normalize();
+            tarDir.y = 0;
+            tarDir.z = 0;
+            gameObject.transform.position += tarDir * followSpeed * Time.deltaTime;
+            float xDiff = Mathf.Abs(gameObject.transform.position.x - Globals.Instance.playerObj.transform.position.x);
+            if(xDiff < .1f)
+            {
+                following = false;
+            }
+        }
 
         CheckLose();
     }
