be8f1d39d5ebfe7c72f45f6b5c029b908234405c
wthrwthr <willrenfroe@gmail.com>
2019-01-26 14:26:28 -0500

    Player Jumping

--- BongoCat/Assets/Scripts/PlayerController.cs
+++ BongoCat/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    [SerializeField]
+    public float vJumpForce = 10f;
+    [SerializeField]
+    public float hJumpForce = 5f;
+
     delegate void action_t();
     List<action_t> actions;
 
-	// Use this for initialization
-	void Start () {
+    private PlayerJump jumpScript;
+
+    // Use this for initialization
+    void Start () {
 
         actions = new List<action_t>();
         actions.Add(JumpLeft);
         actions.Add(JumpRight);
         actions.Add(DoubleJump);
 
+        //Get the the attached gameobject's PlayerJump script and store it in the jumpScript variable.
+
+        jumpScript = this.GetComponent<PlayerJump>();
+
         InputManager foo = InputManager.Instance;
+
     }
 	
 	// Update is called once per frame
@@ -37,11 +49,19 @@ public class PlayerController : MonoBehaviour {
 
     void JumpLeft()
     {
+        //Create a Vector3 that uses the horizontal and vertical jump force parameters.
+        Vector3 lJumpForce = new Vector3(hJumpForce, vJumpForce);
+        //Call the jumpScript's Jump function and pass in the applicable JumpForce Vector3.
+        jumpScript.Jump(lJumpForce);
         Debug.Log(Time.time + " JumpLeft");
     }
 
     void JumpRight()
     {
+        //Create a Vector3 that uses the negative horizonal and regular vertical jump force parameters.
+        Vector3 rJumpForce = new Vector3(-hJumpForce, vJumpForce);
+        //Call the jumpScript's Jump function and pass in the applicable JumpForce Vector3.
+        jumpScript.Jump(rJumpForce);
         Debug.Log(Time.time + " JumpRight");
     }
 
---
+++ BongoCat/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Jump (Vector3 jumpForce)
+    {
+        rb.AddForce(jumpForce, ForceMode.Impulse);
+    }
+}
