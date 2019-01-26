c94e803fae1f0d773a9fcc6fbfb9b921ffc039ba
Thundercleez <tmizas@gmail.com>
2019-01-26 18:57:45 -0500

    Added win condition.

---
+++ BongoCat/Assets/Scripts/CatHouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatHouse : MonoBehaviour {
+
+    public GameObject winText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            Globals.Instance.playerObj.GetComponent<PlayerController>().enabled = false;
+            Globals.Instance.mainCam.gameObject.GetComponent<CamController>().enabled = false;
+            winText.SetActive(true);
+        }
+    }
+}
---
+++ BongoCat/Assets/Scripts/WinScreenController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreenController : MonoBehaviour
+{
+
+    delegate void action_t();
+    List<action_t> actions;
+
+    // Use this for initialization
+    void Start()
+    {
+        actions = new List<action_t>();
+        actions.Add(LoadNextLevel);
+        actions.Add(LoadNextLevel);
+
+        InputManager foo = InputManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void ProcessInputs()
+    {
+        for (int i = 0; i < InputManager.Instance.inputEntries.Count; i++)
+        {
+            if (!InputManager.Instance.inputEntries[i].processed)
+            {
+                InputManager.Instance.inputEntries[i].processed = true;
+                actions[(int)InputManager.Instance.inputEntries[i].inputAction]();
+            }
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel); //TODO
+    }
+}
